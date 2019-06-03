5dd8952b82319ad2fbc154a8c58d5dbba8d54f0e
jamie broberg <angelfire529@yahoo.com>
2019-06-03 09:50:47 -0700

    Updates

---
+++ precache-manifest.837d0d5c310723dd8221ca486152548e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376acbe056a800e1fffe056da47b3608",
+    "url": "/bb_7prototype/index.html"
+  },
+  {
+    "revision": "2dc0bd38d68cd405633a",
+    "url": "/bb_7prototype/static/css/main.e121712b.chunk.css"
+  },
+  {
+    "revision": "d8dcce844d2644e95adc",
+    "url": "/bb_7prototype/static/js/2.7abb4a0e.chunk.js"
+  },
+  {
+    "revision": "2dc0bd38d68cd405633a",
+    "url": "/bb_7prototype/static/js/main.23139083.chunk.js"
+  },
+  {
+    "revision": "7877ab89c5e2cd4a1dff",
+    "url": "/bb_7prototype/static/js/runtime~main.198d89e8.js"
+  },
+  {
+    "revision": "907b74de12b89805612e69cd9ece33f6",
+    "url": "/bb_7prototype/static/media/ElegantIcons.907b74de.svg"
+  },
+  {
+    "revision": "d72ad3f702b9f23540e8ed78b4b65749",
+    "url": "/bb_7prototype/static/media/ElegantIcons.d72ad3f7.eot"
+  },
+  {
+    "revision": "f9d179f59b0878ffcd32a5b3c8ae9c62",
+    "url": "/bb_7prototype/static/media/ElegantIcons.f9d179f5.ttf"
+  },
+  {
+    "revision": "fdd9e757bf61675343dcf55100422b84",
+    "url": "/bb_7prototype/static/media/ElegantIcons.fdd9e757.woff"
+  }
+]);
